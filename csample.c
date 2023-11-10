a731ba203e38e2e31b9cab3972834cf6c5c86b6b
J$ <jeroen.scheerder@on2it.net>
2023-11-10 17:04:52 +0100

    1

--- csample.c
+++ csample.c
@@ -4,10 +4,8 @@
 
 #define len 256
 
-
 int main(int argc, char **argv) {
-    int max=0, maxelf=0;
-    int elf=1, count=0;
+    int max=0, maxelf=0, elf=1, count=0;
     char line[len];
 
     while (fgets(line,len,stdin)) {
@@ -20,7 +18,7 @@ int main(int argc, char **argv) {
             elf++;
             count=0;
         } else {
-            count+= (int)strtol(line, (char **)NULL, 10);
+            count+=(int)strtol(line, (char **)NULL, 10);
         }
     }
     printf("Elf %i carries most calories: %i\n", maxelf, max);
