903deae99c823fc8566c801a423156e7dd316ebe
J$ <jeroen.scheerder@on2it.net>
2023-12-04 13:06:55 +0100

    1

---
+++ day01/day1.1.swift
@@ -0,0 +1,25 @@
+#!/usr/bin/env swift -enable-bare-slash-regex
+
+let simpleDigits = /([0-9])/
+
+var inputS: [String] = []
+var sum = 0
+
+while let input = readLine() {
+    if !inputS.contains(input) {
+        inputS.append(input)
+    }
+}
+
+for l in inputS {
+    var num=0
+    if let match = l.firstMatch(of: simpleDigits) {
+        num = Int(match.1) ?? 0
+        sum = sum + num * 10
+    }
+    if let match = String(l.reversed()).firstMatch(of: simpleDigits) {
+        num = Int(match.1) ?? 0
+        sum = sum + num
+    }
+}
+print("Total: \(sum).")
